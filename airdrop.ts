b7f98dc6f6cb909278d0c9b6cfdd6492724c046b
sebatustra <sebatustra@protonmail.com>
2024-06-27 16:10:48 -0400

    first commit

---
+++ airdrop.ts
@@ -0,0 +1,24 @@
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import wallet from "./dev-wallet.json";
+
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+(async () => {
+    try {
+        console.log("airdropping to wallet: ", keypair.publicKey.toString())
+
+        const txSig = await connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
+        const latestBlockHash = await connection.getLatestBlockhash();
+
+        await connection.confirmTransaction({
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            signature: txSig
+        });
+
+        console.log(`https://explorer.solana.com/tx/${txSig}?cluster=devnet`)
+    } catch(e) {
+        console.error("error: ", e)
+    }
+})();
---
+++ keygen.ts
@@ -0,0 +1,6 @@
+import { Keypair } from "@solana/web3.js";
+
+const keypair = Keypair.generate();
+
+console.log(`You've generated a new Solana wallet: ${keypair.publicKey.toBase58()}`);
+console.log(`Solana Wallet Secret Key: ${keypair.secretKey}]`);
